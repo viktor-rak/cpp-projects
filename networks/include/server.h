608d34b5d121a1e676cfee1d462dc2ed829cffa6
viktor-rak <vrakov2012@gmail.com>
2025-03-02 17:39:47 -0500

    learning how constructors work

--- networks/include/server.h
+++ networks/include/server.h
@@ -2,19 +2,30 @@
 #define SERVER
 
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string>
 
 class Server {
     public:
-    Server(std::string addr, int port);
+    Server(std::string addr, int port); 
     ~Server();
+
+    Server(const Server&) = delete;  //disable copy constructor and shallow copying 
+    Server& operator=(const Server&) = delete;
+    
+    Server(const Server&) noexcept; //move semantics
+    Server& operator=(Server&& other) noexcept;
+
     int startServer();
+    void closeServer();
+    int acceptClient();
 
     private:
     int serverSocket;
     struct sockaddr_in serverAddress;
 
+    int port;
 
 };
 
